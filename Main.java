d5342444e29e06f4820dba9d28b5fc43c71170cd
Shantanu0107 <126323117+Shantanu0107@users.noreply.github.com>
2023-02-27 14:38:56 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,9 +11,12 @@ public class Main {
     public static void main(String args[]) {
      
         Model model = new Model();               // Create shared database
-        Controller1 c1 = new Controller1(model); // Create first controller
-        Controller2 c2 = new Controller2(model); // Create second controller
-        
+        Controller1 c1 = new Controller1(model, "Controller 1",40,40); // Create first controller
+       
+        Controller2 c2 = new Controller2(model, "Controller 2 (A)",400,40); // Create second controller
+        Controller2 c3 = new Controller2(model,"Controller 2 (B)",40,200); // Create second controller
+        Controller2 c4 = new Controller2(model,"Controller 2 (C)",400,200); // Create second controller
+       
     } // main
-  
+ 
 } // Main
